1ab8f724b439c26abd5d82eecf26de1b7ac435bd
enricomizzi <272875@studenti.unimore.it>
2024-04-05 19:11:57 +0200

    initial commit

--- server0.py
+++ server0.py
@@ -36,6 +36,6 @@ def get_data(s):
         return 'sensor not found', 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='127.0.0.1', port=80, debug=True)
 
     #http://localhost/static/graph.html
