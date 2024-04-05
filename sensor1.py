9eb68935d0445c8eb07d251d5531a0a5aefb428c
enricomizzi <272875@studenti.unimore.it>
2024-04-05 19:16:30 +0200

    initial commit

--- sensor1.py
+++ sensor1.py
@@ -1,18 +1,15 @@
 from requests import get, post
 import time
 
-
-base_url = 'http://10.198.0.3:80'
-
+base_url = 'http://localhost:80'
 sensor = 's1'
-with open("CleanData_PM10.csv") as f:
+with open('CleanData_PM10.csv') as f:
     for l in f.readlines()[1:]:
-        print(l)
-        data,val= l.strip().split(",")
+        data,val = l.strip().split(',')
         print(data,val)
-        r = post(f'{base_url}/sensors/{sensor}', data={"data": data, 'val': val})
+        r = post(f'{base_url}/sensors/{sensor}',
+                 data={'data':data,'val':val})
         time.sleep(5)
-
 
 
 print('done')
--- server0.py
+++ server0.py
@@ -1,41 +1,42 @@
-from flask import Flask,request, redirect, url_for
+from flask import Flask,request,redirect,url_for
 import json
 
 app = Flask(__name__)
 
-db={}
+db = {}
 
-@app.route('/graph') #quello che rappresenta l'url che vogliamo esporre
+@app.route('/graph', methods=['GET'])
 def graph():
-    redirect(url_for("static", filename='graph.html'))
+    return redirect(url_for('static', filename='graph.html'))
+
+
 
 @app.route('/sensors',methods=['GET'])
 def sensors():
     return json.dumps(list(db.keys())), 200
+
 
 @app.route('/sensors/<s>',methods=['POST'])
 def add_data(s):
     data = request.values['data']
     val = float(request.values['val'])
     if s in db:
-        db[s].append([data,val])
+        db[s].append((data,val))
     else:
-        db[s] = [[data,val]]
-    print(data)
+        db[s] = [(data,val)]
     return 'ok',200
 
 @app.route('/sensors/<s>',methods=['GET'])
 def get_data(s):
     if s in db:
-        # return json.dumps(db[s]) versione migliore
+        # return json.dumps(db[s])
         r = []
         for i in range(len(db[s])):
-            r.append([i, db[s][i][1]])
-        return json.dumps(r), 200
+            r.append([i,db[s][i][1]])
+        return json.dumps(r),200
     else:
-        return 'sensor not found', 404
+        return 'sensor not found',404
+
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=80, debug=True)
-
-    #http://localhost/static/graph.html
+    app.run(host='0.0.0.0', port=80, debug=True)
