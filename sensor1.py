3ed8dbca3524047a006049fe24968976abbf7738
enricomizzi <272875@studenti.unimore.it>
2024-04-06 10:46:40 +0200

    initial commit

--- sensor1.py
+++ sensor1.py
@@ -1,7 +1,7 @@
 from requests import get, post
 import time
 
-base_url = 'http://127.0.0.1:80'
+base_url = 'http://localhost:80'
 sensor = 's1'
 with open('CleanData_PM10.csv') as f:
     for l in f.readlines()[1:]:
