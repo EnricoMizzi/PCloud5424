6fca008340565282ccfc7d8279480444e83cb7b8
enricomizzi <272875@studenti.unimore.it>
2024-04-11 17:58:02 +0200

    initial commit

--- sensor1.py
+++ sensor1.py
@@ -1,7 +1,8 @@
 from requests import get, post
 import time
 
-base_url = 'http://34.154.119.196:80'
+#base_url = 'http://34.154.119.196:80'
+base_url = 'https://pycloud2024.ew.r.appspot.com'
 sensor = 's1'
 with open('CleanData_PM10.csv') as f:
     for l in f.readlines()[1:]:
